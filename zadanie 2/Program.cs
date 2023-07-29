5c5f9f3ab2dfc8dee047462d3d9fc46459c1e30f
Pavel <pashaslovev.@mail.com>
2023-07-29 18:12:34 +1000

    zadanie 2

---
+++ zadanie 2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да 
+//a = 2, b = 10 -> нет 
+//a = 9, b = -3 -> да 
+//a = -3 b = 9 -> нет
+
+Console.WriteLine ("Enter num : ");
+string strNum1 = Console.ReadLine();
+Console.WriteLine ("Enter num : ");
+string strNum2 = Console.ReadLine();
+int num1 = int.Parse(strNum1);
+int num2 = int.Parse(strNum2);
+int square = num2 * num2 ;
+if(square== num1)
+{
+Console.WriteLine($"Да число {num1} является квадратом {square}");    
+}
+else
+{
+Console.WriteLine($"Нет число {num1} не является квадратом {square}");    
+}
--- zadanie 2/obj/Debug/net8.0/zadanie 2.AssemblyInfo.cs
+++ zadanie 2/obj/Debug/net8.0/zadanie 2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("seminar1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("zadanie 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86930a0014d1ba02a965d6c89abe52696ab6f982")]
-[assembly: System.Reflection.AssemblyProductAttribute("seminar1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("seminar1")]
+[assembly: System.Reflection.AssemblyProductAttribute("zadanie 2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("zadanie 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
